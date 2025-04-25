4e04aca7d4473bc2eb193babd1cbe68bf410effc
Bhushan Bhaiswar <bhaiswarbhushan50@gmail.com>
2025-04-25 23:51:34 +0530

    Initial commit - Flask app

--- app.py
+++ app.py
@@ -106,4 +106,6 @@ def student_dashboard():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Needed for Heroku deployment
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
